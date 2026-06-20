585cdc5f82f607c5e55aea2df8a853967bf24f98
Maks Awtuch <s33657@pjwstk.edu.pl>
2026-06-20 09:56:09 +0200

    first commit

---
+++ src/main/java/org/example/jazs33657nbp/JazS33657NbpApplication.java
@@ -0,0 +1,13 @@
+package org.example.jazs33657nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS33657NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS33657NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jazs33657nbp/JazS33657NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jazs33657nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS33657NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
